f0e5fd8bff698a86bf63d20511865011d4915700
1040103725 <1040103725@qq.com>
2019-03-19 09:56:06 +0800

    上线部署(重写)

--- masonry/static/js/jiaoben.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $('img').each(function () {
-        var imgpath = $(this).attr('src').slice(3,)
-
-        imgPath = "{% static '" + imgpath + "' %}"
-        $(this).attr('src',imgPath)
-    })
-    console.log($('body').html())
-})
